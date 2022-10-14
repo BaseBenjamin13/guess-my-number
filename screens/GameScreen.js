a9ebb607b63a1f0ae0406fb51fa10ed9f0d3e605
BaseBenjamin13 <benmorgiewicz@gmail.com>
2022-10-13 20:48:46 -0400

    setting up game screen

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -2,10 +2,23 @@ import { View, Text, StyleSheet } from 'react-native';
 
 function GameScreen() {
   return (
-    <View >
-        <Text>Game Screen</Text>
+    <View style={styles.screen}>
+        <Text>Opponent's Guess</Text>
+
+        <View>
+            <Text>Higher or Lower?</Text>
+
+        </View>
+        <View></View>
     </View>
   )
 }
 
-export default GameScreen
+export default GameScreen;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12,
+    }
+});
